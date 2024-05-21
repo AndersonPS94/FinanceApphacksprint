f2135fcba54d74f265b262a4944afec5a5759132
AndersonPS94 <dson_adn@hotmail.com>
2024-05-21 02:05:06 -0300

    criação da Application

---
+++ app/src/main/java/com/example/financeapphacksprint/FinanceAppApplication.kt
@@ -0,0 +1,25 @@
+package com.example.financeapphacksprint
+
+import android.app.Application
+import androidx.room.Database
+import androidx.room.Room
+import com.example.financeapphacksprint.data.AppDataBase
+
+
+class FinanceAppApplication : Application() {
+
+    private lateinit var dataBase:AppDataBase
+
+    override fun onCreate() {
+        super.onCreate()
+        dataBase = Room.databaseBuilder(
+            applicationContext,
+            AppDataBase::class.java, "finance_app_db"
+        ).build()
+    }
+
+    fun getAppDataBase(): AppDataBase {
+        return dataBase
+    }
+    // Chamar a função do banco de dados para acessar o banco de dados da aplicação
+}
