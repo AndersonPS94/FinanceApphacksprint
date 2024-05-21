8ea6211911ba2da002c803ac68735473b6c08f80
AndersonPS94 <dson_adn@hotmail.com>
2024-05-21 00:56:45 -0300

    dropdown menu funcionando (aprimorar o design nos proximos updates)

--- app/src/main/java/com/example/financeapphacksprint/Create_Outcome.kt
+++ app/src/main/java/com/example/financeapphacksprint/Create_Outcome.kt
@@ -1,6 +1,10 @@
 package com.example.financeapphacksprint
 
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +15,19 @@ class Create_Outcome : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_create_outcome)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val items = listOf("All","House","Car","Food")
+
+        val autoComplete: AutoCompleteTextView = findViewById(R.id.auto_complete)
+        val adapter = ArrayAdapter(this, R.layout.list_item, items)
+
+        autoComplete.setAdapter(adapter)
+
+        autoComplete.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, i, l ->
+
+            val itemSelected = adapterView.getItemAtPosition(i)
+            Toast.makeText(this, "Selected: $itemSelected", Toast.LENGTH_SHORT).show()
+
         }
     }
 }
